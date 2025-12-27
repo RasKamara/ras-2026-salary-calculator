fed92c31036c5576206d906223d55f32031c9bfd
RasKamara <129194460+RasKamara@users.noreply.github.com>
2025-12-27 13:53:50 +0000

    Add files via upload

---
+++ service-worker[1].js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "ras-2026-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
